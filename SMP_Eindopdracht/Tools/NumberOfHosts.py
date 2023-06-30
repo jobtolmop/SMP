b0a67d6aad13d3e9b226a6c2c4a96b8412df46da
Everardus Tol <jobautodrop@gmail.com>
2023-06-30 15:39:06 +0200

    Added script number of hosts

--- SMP_Eindopdracht/Tools/NumberOfHosts.py
+++ SMP_Eindopdracht/Tools/NumberOfHosts.py
@@ -7,3 +7,30 @@
 
 # Alle verschillende ip-adressen worden geprint met het aanroepen van de functie, 
 # en de laatste print zal bestaan uit ‘er zijn X aantal verschillende ip-adressen in de dataset’
+
+import json
+from collections import Counter
+
+def amount_of_unique_hosts(json_data):
+    unique_hosts = []
+
+    for packet in json_data:
+        src_host = packet['_source']['layers']['ip']['ip.src_host']
+        dst_host = packet['_source']['layers']['ip']['ip.dst_host']
+
+        if src_host not in unique_hosts:
+            unique_hosts.append(src_host)
+        if dst_host not in unique_hosts:
+            unique_hosts.append(dst_host)
+
+    return len(unique_hosts)
+
+def main():
+    with open("dataset.json", "r") as file:
+        data = json.load(file)
+
+    num_unique_hosts = amount_of_unique_hosts(data)
+    print("Amount of different hosts:", num_unique_hosts)
+
+main()
+
